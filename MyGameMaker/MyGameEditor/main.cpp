deb2f5a31b0c3c4699d0f0e18bd8230acee5908c
joanmarquesbesses <85459149+joanmarquesbesses@users.noreply.github.com>
2024-10-27 22:39:41 +0100

    bakerhouse2 vertex_pos and vertex_texCoords
    another bakerhouse but with a vertexbuffer for each data (pos and texCoords). Now having a buferID for pos and another or TexCoords. Ask Pep which is best.

--- MyGameMaker/MyGameEditor/main.cpp
+++ MyGameMaker/MyGameEditor/main.cpp
@@ -140,7 +140,141 @@ struct BakerHouse {
 
 };
 
+struct BakerHouse2 {
+	Transform transform;
+
+	vector<vector<vec3>> vertex_positions;
+	vector<vector<vec2>> vertex_texCoords;
+	vector<vector<unsigned int>> index_data;
+
+	std::unique_ptr<Shader> m_Shader;
+	vector<unsigned int> vBPosID, vBTCoordsID, iBID, vA;
+	std::unique_ptr<Texture> m_Texture;
+
+	unsigned int numM = 0;
+
+	const aiScene* scene = nullptr;
+
+	void LoadFBX(const std::string& pFile) {
+		// Create an instance of the Importer class
+		Assimp::Importer importer;
+
+		// And have it read the given file with some example postprocessing
+		// Usually - if speed is not the most important aspect for you - you'll
+		// probably to request more postprocessing than we do in this example.
+		const aiScene* scene = importer.ReadFile(pFile,
+			aiProcess_CalcTangentSpace |
+			aiProcess_Triangulate |
+			aiProcess_JoinIdenticalVertices |
+			aiProcess_SortByPType);
+
+		// If the import failed, report it
+		if (nullptr == scene) {
+			cout << importer.GetErrorString() << endl;
+		}
+		else {
+
+			numM = scene->mNumMeshes;
+			vertex_positions.resize(numM);
+			vertex_texCoords.resize(numM);
+			index_data.resize(numM);
+
+			for (unsigned int i = 0; i < numM; i++) {
+				aiMesh* mesh = scene->mMeshes[i];
+				std::cout << "Malla " << i << ": " << mesh->mName.C_Str() << std::endl;
+
+				for (unsigned int j = 0; j < mesh->mNumVertices; j++) {
+					aiVector3D vertex = mesh->mVertices[j];
+					vec3 aux;
+					aux.x = vertex.x;
+					aux.y = vertex.y;
+					aux.z = vertex.z;
+					vertex_positions[i].push_back(aux);
+
+					// Coordenadas UV (si existen)
+					if (mesh->mTextureCoords[0]) {  // Comprueba si hay UVs
+						aiVector3D uv = mesh->mTextureCoords[0][j];
+						aux.x = uv.x;  // Solo X y Y
+						aux.y = 1.0f - uv.y;
+					}
+					vertex_texCoords[i].push_back(aux);
+				}
+
+				for (unsigned int j = 0; j < mesh->mNumFaces; j++) {
+					aiFace face = mesh->mFaces[j];
+					for (unsigned int k = 0; k < face.mNumIndices; k++) {
+						index_data[i].push_back(face.mIndices[k]);
+					}
+				}
+
+				unsigned int auxVBPosID, auxVBTCoordsID, auxIBID, auxVA;
+
+				glGenVertexArrays(1, &auxVA);
+				glBindVertexArray(auxVA);
+
+				//buffer de positions
+				glGenBuffers(1, &auxVBPosID);
+				glBindBuffer(GL_ARRAY_BUFFER, auxVBPosID);
+				glBufferData(GL_ARRAY_BUFFER, vertex_positions[i].size() * sizeof(vec3), vertex_positions[i].data(), GL_STATIC_DRAW);
+
+				//position layout
+				glEnableVertexAttribArray(0);
+				glVertexAttribPointer(0, 3, GL_DOUBLE, GL_FALSE, sizeof(vec3), (const void*)0);
+
+				//buffer de coordenades de textura
+				glGenBuffers(1, &auxVBTCoordsID);
+				glBindBuffer(GL_ARRAY_BUFFER, auxVBTCoordsID);
+				glBufferData(GL_ARRAY_BUFFER, vertex_texCoords[i].size() * sizeof(vec3), vertex_texCoords[i].data(), GL_STATIC_DRAW);
+
+				//tex coord layout
+				glEnableVertexAttribArray(1);
+				glVertexAttribPointer(1, 2, GL_DOUBLE, GL_FALSE, sizeof(vec2), (const void*)0);
+
+				glCreateBuffers(1, &auxIBID);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, auxIBID);
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, index_data[i].size() * sizeof(unsigned int), index_data[i].data(), GL_STATIC_DRAW);
+
+				glBindBuffer(GL_ARRAY_BUFFER, 0);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+				glBindVertexArray(0);
+
+				vBPosID.push_back(auxVBPosID);			//buffer de posicions
+				vBTCoordsID.push_back(auxVBTCoordsID);  //buffer de coordenades de textura
+				iBID.push_back(auxIBID);			    //buffer d'indexs
+				vA.push_back(auxVA);					//vertex array
+			}
+
+			m_Shader = std::make_unique<Shader>("../MyGameEngine/Basic.shader");
+
+		}
+	}
+
+	void draw() const {
+
+		for (unsigned int i = 0; i < numM; i++) {
+			glBindVertexArray(vA[i]);
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iBID[i]);
+
+			m_Texture->Bind();
+
+			m_Shader->Bind();
+			m_Shader->SetUniformMat4f("u_MVP", (glm::mat4)camera.projection() * (glm::mat4)camera.view() * (glm::mat4)transform.mat());
+			m_Shader->SetUniform1i("u_Texture", 0);
+
+			glDrawElements(GL_TRIANGLES, index_data[i].size(), GL_UNSIGNED_INT, nullptr);
+
+			m_Shader->UnBind();
+			m_Texture->Unbind();
+
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+			glBindVertexArray(0);
+		}
+	}
+
+};
+
 static BakerHouse bakerHouse;
+static BakerHouse2 bakerHouse2;
 
 static void drawFloorGrid(int size, double step) {
 	glColor3ub(0, 0, 0);
@@ -164,7 +298,8 @@ static void display_func() {
 	drawFloorGrid(16, 0.25);
 
 	//draw house
-	bakerHouse.draw();
+	//bakerHouse.draw();
+	bakerHouse2.draw();
 }
 
 static void init_opengl() {
@@ -197,9 +332,12 @@ int main(int argc, char* argv[]) {
 	camera.transform().rotate(glm::radians(180.0), vec3(0, 1, 0));
 
 	// Load house
-	bakerHouse.m_Texture = std::make_unique<Texture>("../MyGameEngine/Baker_house.png");
+	/*bakerHouse.m_Texture = std::make_unique<Texture>("../MyGameEngine/Baker_house.png");
 	bakerHouse.LoadFBX("../MyGameEngine/BakerHouse.fbx");
-	bakerHouse.transform.pos() = vec3(0, 0, 0);
+	bakerHouse.transform.pos() = vec3(0, 0, 0);*/
+	bakerHouse2.m_Texture = std::make_unique<Texture>("../MyGameEngine/Baker_house.png");
+	bakerHouse2.LoadFBX("../MyGameEngine/BakerHouse.fbx");
+	bakerHouse2.transform.pos() = vec3(0, 0, 0);
 
 	SDL_Event event;
 
