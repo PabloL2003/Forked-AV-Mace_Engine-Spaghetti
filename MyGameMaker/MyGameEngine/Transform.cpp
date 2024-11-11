2c12009c4f6b5d6e08886d4b41c6dab078feffd0
pcelaya <125652831+pcelaya@users.noreply.github.com>
2024-11-11 11:02:35 +0100

    Rotation now dos not change the Transform

--- MyGameMaker/MyGameEngine/Transform.cpp
+++ MyGameMaker/MyGameEngine/Transform.cpp
@@ -11,6 +11,16 @@ Transform::Transform(const mat4& mat) : _mat(mat)
 
 void Transform::rotate(const vec3& eulerAngles) 
 {
+	vec3 currentTransform = _mat[3]; // Translation is stored in the last column
+	vec3 currentScale = glm::vec3(glm::length(_mat[0]), glm::length(_mat[1]), glm::length(_mat[2])); // Scale is the length of the columns;
+
 	_rot = glm::quat(eulerAngles);
-	_mat = glm::mat4_cast(_rot);
+	_mat = glm::mat4(1.0f);
+
+	_mat[0] = glm::vec4(currentScale.x, 0.0f, 0.0f, 0.0f);
+	_mat[1] = glm::vec4(0.0f, currentScale.y, 0.0f, 0.0f);
+	_mat[2] = glm::vec4(0.0f, 0.0f, currentScale.z, 0.0f);
+
+	_mat = mat4(glm::mat4_cast(_rot)) * _mat;
+	_mat[3] = glm::vec4(currentTransform, 1.0f);
 }
